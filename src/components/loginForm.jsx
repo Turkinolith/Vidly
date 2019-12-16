936cb9ed3fbc6ff9552ee97899aed4ee2b5771d9
Larry <50931759+Turkinolith@users.noreply.github.com>
2019-12-15 22:07:19 -0700

    Added autofocus for the username field

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,13 +1,25 @@
 import React, { Component } from "react";
 class LoginForm extends Component {
+  handleSubmit = event => {
+    event.preventDefault();
+
+    // TODO: call the server, save the changes, and then redirect the user to a different page.
+    console.log("Submitted");
+  };
+
   render() {
     return (
       <div>
         <h1>Login</h1>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <div className="form-group">
             <label htmlFor="username">Username</label>
-            <input id="username" type="text" className="form-control" />
+            <input
+              autoFocus
+              id="username"
+              type="text"
+              className="form-control"
+            />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
