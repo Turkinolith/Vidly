d45faaeb11a41460b5507efbfd97d867c2945bf0
Larry <50931759+Turkinolith@users.noreply.github.com>
2019-12-18 13:04:50 -0700

    Added displayed validation errors

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -21,8 +21,7 @@ class LoginForm extends Component {
     event.preventDefault();
 
     const errors = this.validate();
-    console.log(errors);
-    this.setState({ errors });
+    this.setState({ errors: errors || {} });
     if (errors) return;
     // TODO: call the server, save the changes, and then redirect the user to a different page.
     console.log("Submitted");
@@ -35,7 +34,7 @@ class LoginForm extends Component {
   };
 
   render() {
-    const { account } = this.state;
+    const { account, errors } = this.state;
 
     return (
       <div>
@@ -46,12 +45,14 @@ class LoginForm extends Component {
             value={account.username}
             label="Username"
             onChange={this.handleChange}
+            error={errors.username}
           />
           <Input
             name="password"
             value={account.password}
             label="Password"
             onChange={this.handleChange}
+            error={errors.password}
           />
           <button className="btn btn-primary">Login</button>
         </form>
